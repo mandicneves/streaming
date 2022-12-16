79dd1a076fb3763ca27977056448a16bd7fabb33
Mandic <mandicneves@gmail.com>
2022-12-16 14:39:36 -0300

    Changes in receiver

--- 1.0v/receiver.py
+++ 1.0v/receiver.py
@@ -4,7 +4,7 @@ import threading
 # host (IP addres), port, slot(how many can connect)
 receiver = StreamingServer('192.168.0.136', 9999)
 
-t = threading.Thread(target=receiver.start_serve)
+t = threading.Thread(target=receiver.start_server)
 t.start()
 
 while input("") != "STOP":
